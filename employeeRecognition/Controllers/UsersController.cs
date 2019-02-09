f89614f4e52ec3536d6256919fea279cfe25ef41
mcastillo40 <chgreen08@gmail.com>
2019-02-09 09:40:08 -0800

    Add database connection class

--- employeeRecognition/Controllers/UsersController.cs
+++ employeeRecognition/Controllers/UsersController.cs
@@ -5,39 +5,34 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using employeeRecognition.Extensions;
 
 namespace employeeRecognition.Controllers
 {
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
-        string connectionString = @"Data Source = tcp:erraisqlserver.database.windows.net,1433;Initial Catalog=EmployeeDB;Persist Security Info=False;User ID=erraiadmin;Password=DBaccess3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        DataTable dt { get; set; }
+
+        DbConnection sqlConnection = new DbConnection();
 
         [HttpGet("[action]")]
         public IEnumerable<Models.UserAcct> Index()
         {
             List<Models.UserAcct> list = new List<Models.UserAcct>();
 
-            // runs stored procedure and returns data to main page
-            using (SqlConnection con = new SqlConnection(connectionString))
+            String sql = @"SELECT * FROM userAcct";
+
+            dt = sqlConnection.Connection(sql);
+
+            foreach (DataRow row in dt.Rows)
             {
-                String sql = @"SELECT * FROM userAcct";
-
-                DataTable dt = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter();
-                da.SelectCommand = new SqlCommand(sql, con);
-
-                da.Fill(dt);
-
-                foreach (DataRow row in dt.Rows)
-                {
-                    var user = new Models.UserAcct();
-                    user.id = (int)row["id"];
-                    user.first_name = row["first_name"].ToString();
-                    user.last_name = row["last_name"].ToString();
-                    user.email = row["email"].ToString();
-                    list.Add(user);
-                }
+                var user = new Models.UserAcct();
+                user.id = (int)row["id"];
+                user.first_name = row["first_name"].ToString();
+                user.last_name = row["last_name"].ToString();
+                user.email = row["email"].ToString();
+                list.Add(user);
             }
 
             return list;
---
+++ employeeRecognition/Extensions/DbConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace employeeRecognition.Extensions
+{
+    public class DbConnection
+    {
+        private string connectionString = @"Data Source = tcp:erraisqlserver.database.windows.net,1433;Initial Catalog=EmployeeDB;Persist Security Info=False;User ID=erraiadmin;Password=DBaccess3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        private DataTable dt { get; set; }
+
+        // runs stored procedure and returns data to main page
+        public DataTable Connection(String sql)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = new SqlCommand(sql, con);
+
+            da.Fill(dt);
+
+            return dt;
+        }
+    }
+}
