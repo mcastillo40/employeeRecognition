a8a1106e2df75331729ca63673fc870b1fe71280
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-02-14 11:17:19 -0800

    Reroute login signup forget password. Changed Home

--- employeeRecognition/ClientApp/src/components/Home.js
+++ employeeRecognition/ClientApp/src/components/Home.js
@@ -6,12 +6,20 @@ export class Home extends Component {
   render() {
     return (
       <div>
+        <h1>Welcome to the Employee Recognition Portal!</h1>
+         
 
-        <h1>Welcome, </h1>
-        </div>
+            <p>Welcome to your new single-page application, built with:</p>
+ <div class="row">
+             <div class="col-md-6">
+                         <a href="/users" class="btn btn-primary btn-block">View Accounts</a>
+             </div>
 
+             <div class="col-md-6">
+                         <a href="/Award" class="btn btn-primary btn-block">View Awards</a>
+                    </div>
+             </div>
+</div>
     );
   }
 }
-
-
