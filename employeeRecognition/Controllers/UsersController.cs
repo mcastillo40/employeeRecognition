0e2c99fbb9d86c17c1fdd526796344a677218469
laigOSU <laig@oregonstate.edu>
2019-02-14 09:49:33 -0800

    demo jwt fixed build

--- employeeRecognition/Controllers/UsersController.cs
+++ employeeRecognition/Controllers/UsersController.cs
@@ -5,35 +5,39 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using employeeRecognition.Extensions;
 
 namespace employeeRecognition.Controllers
 {
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
-        private DataTable dt { get; set; }
-
-        private DbConnection sqlConnection = new DbConnection();
+        string connectionString = @"Data Source = tcp:erraisqlserver.database.windows.net,1433;Initial Catalog=EmployeeDB;Persist Security Info=False;User ID=erraiadmin;Password=DBaccess3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
 
         [HttpGet("[action]")]
         public IEnumerable<Models.UserAcct> Index()
         {
             List<Models.UserAcct> list = new List<Models.UserAcct>();
 
-            String sql = @"SELECT * FROM userAcct";
-
-            dt = sqlConnection.Connection(sql);
-
-            foreach (DataRow row in dt.Rows)
+            // runs stored procedure and returns data to main page
+            using (SqlConnection con = new SqlConnection(connectionString))
             {
-                var user = new Models.UserAcct();
-                user.id = (int)row["id"];
-                user.first_name = row["first_name"].ToString();
-                user.last_name = row["last_name"].ToString();
-                user.role = (int)row["role"];
-                user.email = row["email"].ToString();
-                list.Add(user);
+                String sql = @"SELECT * FROM userAcct";
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = new SqlCommand(sql, con);
+
+                da.Fill(dt);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    var user = new Models.UserAcct();
+                    user.id = (int)row["id"];
+                    user.first_name = row["first_name"].ToString();
+                    user.last_name = row["last_name"].ToString();
+                    user.email = row["email"].ToString();
+                    list.Add(user);
+                }
             }
 
             return list;
