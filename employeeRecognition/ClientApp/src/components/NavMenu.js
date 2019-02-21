9b94ef8ee08e74dee03939ae11fcce5cd41eb36a
operations <operations@mazamamedia.com>
2019-02-21 15:56:02 -0800

    Hide navlinks

--- employeeRecognition/ClientApp/src/components/NavMenu.js
+++ employeeRecognition/ClientApp/src/components/NavMenu.js
@@ -1,20 +1,20 @@
 ﻿import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AUTH_MODEL } from '../Shared/Auth/Auth';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
     displayName = NavMenu.name
 
-    render() {
+    constructor(props) {
+        super(props);
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <NavLink className="navbar-brand" exact to="/">Employee Recognition</NavLink>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        this.NavbarLinks.bind(this);
+    }
 
-            <div className="collapse navbar-collapse" id="navbarContent">
+    NavbarLinks() {
+        if (!_.isNil(AUTH_MODEL.get().token)) {
+            return (
                 <div className="navbar-nav ml-auto">
                     <NavLink className="nav-link nav-item" exact to="/login">Login</NavLink>
                     <NavLink className="nav-link nav-item" exact to="/">Home</NavLink>
@@ -23,8 +23,25 @@ export class NavMenu extends Component {
                     <NavLink className="nav-link nav-item" to="/award">Awards</NavLink>
                     <NavLink className="nav-link nav-item" to="/settings">Settings</NavLink>
                 </div>
-            </div>
-        </nav>
-    );
+            );
+        }
+        else {
+            return (<div></div>)
+        }
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <NavLink className="navbar-brand" exact to="/">Employee Recognition</NavLink>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarContent">
+                    {this.NavbarLinks()}
+                </div>
+            </nav>
+        );
   }
 }
