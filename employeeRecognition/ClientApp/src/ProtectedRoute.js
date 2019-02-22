90018f9789a77b3b4111ac603265364ddef575d2
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-02-21 18:33:51 -0800

    Demo Protected Routes

---
+++ employeeRecognition/ClientApp/src/ProtectedRoute.js
@@ -0,0 +1,31 @@
+﻿import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import auth from './components/auth';
+
+export const ProtectedRoute = ({component: Component, ...rest}) => {
+    return(
+        <Route 
+            {...rest} 
+            render={props => {
+                // Check if user is authenticated
+                if(auth.isAuthenticated()){
+                    return <Component {...props} />;
+                }
+                else {  //Else, redirect user
+                    return <Redirect to={
+                        {
+                            pathname: "/",
+                            state: {
+                                from: props.location
+                            }
+                        }
+
+                    }/>
+                }
+                 
+            
+
+            }}
+        />
+    )
+}
--- employeeRecognition/ClientApp/src/index.js
+++ employeeRecognition/ClientApp/src/index.js
@@ -6,12 +6,31 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { LandingPage } from './components/LandingPage';
+import { Route, Switch } from 'react-router-dom';
+import { AppLayout } from './components/AppLayout';
+import { ProtectedRoute } from './ProtectedRoute';
+
+
+function Splash(){
+    return (
+        <div className="Splash">
+            <Switch>
+                <Route exact path="/" component={LandingPage}/>
+                <ProtectedRoute exact path="/app" component={AppLayout}/>
+                <Route path="*" component = {()=> "404 NOT FOUND"}/>
+            </Switch>
+        </div>
+    )
+}
+
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+    <Splash />
   </BrowserRouter>,
   rootElement);
 
