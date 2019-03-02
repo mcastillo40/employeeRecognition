5e9b5a399d0d9d640546411818af6f7d9441fc5c
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-03-02 00:59:46 -0800

    Connect to DB

--- employeeRecognition/Startup.cs
+++ employeeRecognition/Startup.cs
@@ -4,20 +4,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using JwtAuthentication.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 namespace employeeRecognition
 {
@@ -31,28 +31,34 @@ namespace employeeRecognition
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Introduce JWT token authorization scheme into our application, under startup.cs under method configure services.
         public void ConfigureServices(IServiceCollection services)
         {
 
-            //Medhatel: associate the connection string with the context class
-            services.AddDbContext<ApplicationDbContext>(
-                option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // From AuthController.cs
+            string securityKey = "this_is_the_very_long_security_key_for_token_validation_keep_this_a_secret_somehow";
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
 
-
-            services.AddIdentity<IdentityUser, IdentityRole>(
-                    option =>
+            // Add auto authentication scheme
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        option.Password.RequireDigit = false;
-                        option.Password.RequiredLength = 6;
-                        option.Password.RequireNonAlphanumeric = false;
-                        option.Password.RequireUppercase = false;
-                        option.Password.RequireLowercase = false;
-                    }
-                ).AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+                        // What to validate
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
 
+                        // Setup Validate Data
+                        ValidIssuer = "theIssuer",
+                        ValidAudience = "readers",
+                        IssuerSigningKey = symmetricSecurityKey
+                    };
+                });
 
-            // Original
+            // Set some options to be used to validate our token
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
@@ -76,6 +82,9 @@ namespace employeeRecognition
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
+
+
+            app.UseAuthentication();  // Added for JWT authentication
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
